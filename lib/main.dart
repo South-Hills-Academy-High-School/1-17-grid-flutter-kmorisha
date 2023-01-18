bc7fc4fe3b7b359550fb9e8f06e264a6c2d30c7e
kmoristudent@shacademy.org <kmoristudent@shacademy.org>
2023-01-18 12:00:32 -0800

    working on boiler plate

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+const Myapp({super.key});
+
+@override
+Widget build(BuildContext context) {
+  return ChangeNotifierProvider(
+    create: (context) => MyAppState(),
+    child: MaterialApp(
+      title: '1-17 App',
+      theme: ThemeData(),
+      home: MyHomePage(),
+    ),
+  
+
+
+  );
+
+}
+}
+
+
+
+
+ 
